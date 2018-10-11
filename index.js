078cb2e92fc0edeeaabc6e35c1dd995fbb3c939a
Nael Tasmim <naelandodev@gmail.com>
2018-10-11 13:28:48 +0200

    edit type

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ BrunchPlugin.prototype.brunchPlugin = true;
 
 // Required for compilers, linters & optimizers.
 // 'javascript', 'stylesheet' or 'template'
-//BrunchPlugin.prototype.type = 'c';
+BrunchPlugin.prototype.type = 'stylesheet';
 
 // Required for compilers & linters.
 // It would filter-out the list of files to operate on.
