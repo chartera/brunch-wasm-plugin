32174b351962456b866204a907200603b40e1f90
Nael Tasmim <naelandodev@gmail.com>
2018-10-11 12:53:28 +0200

    -.-

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// Documentation for Brunch plugins:
+// http://brunch.io/docs/plugins
+
+// Remove everything your plugin doesn't need.
+class BrunchPlugin {
+  constructor(config) {
+    // Replace 'plugin' with your plugin's name;
+    this.config = config.plugins.wasm || {};
+  }
+
+  // Optional
+  // Specifies additional files which will be included into build.
+  // get include() { return ['path-to-file-1', 'path-to-file-2']; }
+
+  // file: File => Promise[Boolean]
+  // Called before every compilation. Stops it when the error is returned.
+  // Examples: ESLint, JSHint, CSSCheck.
+  // lint(file) { return Promise.resolve(true); }
+
+  // file: File => Promise[File]
+  // Transforms a file data to different data. Could change the source map etc.
+  // Examples: JSX, CoffeeScript, Handlebars, SASS.
+    compile(file) {
+	console.log("brunch-was plugin", file);
+	return Promise.resolve(file);
+    }
+
+  // file: File => Promise[Array: Path]
+  // Allows Brunch to calculate dependants of the file and re-compile them too.
+  // Examples: SASS '@import's, Jade 'include'-s.
+  // getDependencies(file) { return Promise.resolve(['dep.js']); }
+
+  // file: File => Promise[File]
+  // Usually called to minify or optimize the end-result.
+  // Examples: UglifyJS, CSSMin.
+  // optimize(file) { return Promise.resolve({data: minify(file.data)}); }
+
+  // files: [File] => null
+  // Executed when each compilation is finished.
+  // Examples: Hot-reload (send a websocket push).
+  // onCompile(files) {}
+
+  // Allows to stop web-servers & other long-running entities.
+  // Executed before Brunch process is closed.
+  // teardown() {}
+}
+
+// Required for all Brunch plugins.
+BrunchPlugin.prototype.brunchPlugin = true;
+
+// Required for compilers, linters & optimizers.
+// 'javascript', 'stylesheet' or 'template'
+BrunchPlugin.prototype.type = 'c';
+
+// Required for compilers & linters.
+// It would filter-out the list of files to operate on.
+BrunchPlugin.prototype.extension = 'c';
+BrunchPlugin.prototype.pattern = /\.c$/;
+
+// Indicates which environment a plugin should be applied to.
+// The default value is '*' for usual plugins and
+// 'production' for optimizers.
+// BrunchPlugin.prototype.defaultEnv = 'production';
+
+module.exports = BrunchPlugin;
